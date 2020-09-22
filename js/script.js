04eafdb9d231a7190d4c761807fd4573f7c8c473
marco <marcodecrema@libero.it>
2020-09-22 17:18:37 +0200

    funzione ricerca titolo

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 $(document).ready(function() {
 
-var search = "ritorno al futuro";
+
+
+$("#cerca").click(function() {
+  var search = $("#ricercaFilm").val().toLowerCase();
+  if (search != "") {
+    console.log(search.length);
+  } else {
+    alert("titolo non trovato");
+  }
+  return search;
+})
+
 
   $.ajax({
     "url": "https://api.themoviedb.org/3/search/movie",
     "data": {
       "api_key": "d1917066dde7a9b02484a9c4ced0c18e",
-      "query": search,
+      "query": "ritorno al futuro",
       //"language": "it-IT"
     },
     "method": "GET",
     "success": function(data) {
-      //console.log(data);
-      var data = data.results;
-      filtroFilm(data);
+      console.log(data);
+      /*var data = data.results;
+      filtroFilm(data);*/
     },
     "error": function(errore) {
       alert("errore");
     }
   })
 
+/*
 function filtroFilm(value) {
   for (i=0; i<=value.length; i++) {
       console.log(value[i]);
@@ -42,7 +54,7 @@ function filtroFilm(value) {
 
     $(".row").append(contenuto);
   }
-}
+}*/
 
 
 
