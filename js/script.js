077f63d7ecacd18d6a0daa44e4e0fba5120d38e4
marco <marcodecrema@libero.it>
2020-09-24 13:41:56 +0200

    aggiunta copertina e funzone abbr titolo

--- js/script.js
+++ js/script.js
@@ -24,6 +24,26 @@ var search = $("#ricercaFilm").val().toLowerCase();
   }
   }
 
+// Funzione per abbreviare il titolo nel caso sia più lungo di 30 caratteri
+  function title(titolo) {
+    var nuovoTesto = "";
+    var lunParola = titolo.length;
+      if (lunParola >= 30) {
+        for (var i = 0; i<30; i++) {
+        nuovoTesto += titolo[i];
+      }
+      return nuovoTesto+"...";
+    } else {
+      nuovoTesto += titolo;
+      return nuovoTesto;
+    }
+  }
+
+// Funzione immette la bandiera corrispondente alla nazione della lingua orginale
+  function flag(lingua) {
+    return "img/"+lingua+".svg";
+  }
+
 // Funzione colora stelle a seconda del voto
   function voto(num) {
    var num = Math.round(num / 2);
@@ -98,8 +118,9 @@ function filtroFilm(value) {
       var language = value[i].original_language;
 
     var film = {
-      "titolo": value[i].title,
+      "titolo": title(value[i].title),
       "titoloOriginale": value[i].original_title,
+      "img-cop": "https://image.tmdb.org/t/p/w200/"+value[i].poster_path,
       "img-band": flag(language),
       "lingua": value[i].original_language,
       "valutazione": value[i].vote_average,
@@ -130,13 +151,16 @@ function filtroSeries(value2) {
       console.log(value2[i]);
 
     var language2 = value2[i].original_language;
+    var average = value2[i].vote_average;
 
     var serie = {
-      "titolo": value2[i].name,
+      "titolo": title(value2[i].name),
       "titoloOriginale": value2[i].original_name,
+      "img-cop": "https://image.tmdb.org/t/p/w200/"+value2[i].poster_path,
       "img-band": flag(language2),
+      "lingua": value2[i].original_language,
       "valutazione": value2[i].vote_average,
-
+      "star": voto(average),
       // Classi css
       "previewFilm": "preview",
       "listaInfo": "lista",
@@ -155,11 +179,6 @@ function filtroSeries(value2) {
     $("#ricercaFilm").val("");
     $(this).addClass("active");
   }
-}
-
-// Funzione immette la bandiera corrispondente alla nazione della lingua orginale
-function flag(lingua) {
-  return "img/"+lingua+".svg";
 }
 
 // Animazione menu
@@ -188,6 +207,7 @@ function chiudiMenu() {
   $(".menu").css({marginTop: "0px"});
   $(".nav_bar").css({borderBottom: "8px solid #5A0505"});
 }
+
 
 
   // Fine codice
